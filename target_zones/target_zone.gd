bfabc44ecd74519d589a63f382f528c2b45071a5
mrbartchak <matthewbartchak@gmail.com>
2026-02-18 16:18:21 -0800

    I like this eye thing I got going on

--- target_zones/target_zone.gd
+++ target_zones/target_zone.gd
@@ -6,8 +6,11 @@ var value: int = 1
 var overlapping_blocks: int = 0
 var is_covered: bool = false
 
-@onready var sprite: Sprite2D = $Sprite
-@onready var sprite_filled: Sprite2D = $SpriteFilled
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite
+@onready var sprite_filled: AnimatedSprite2D = $AnimatedSpriteFilled
+
+func _ready() -> void:
+	_blink_loop()
 
 func score() -> int:
 	self.monitoring = false
@@ -43,3 +46,19 @@ func _update_visuals() -> void:
 	else:
 		sprite.show()
 		sprite_filled.hide()
+
+func _blink_loop() -> void:
+	var wait_time: float = randf_range(3.0, 8.0)
+	await get_tree().create_timer(wait_time).timeout
+	_blink()
+	await sprite.animation_finished
+	_idle()
+	_blink_loop()
+
+func _blink() -> void:
+	sprite.play("blink")
+	sprite_filled.play("blink")
+
+func _idle() -> void:
+	sprite.play("default")
+	sprite_filled.play("default")
